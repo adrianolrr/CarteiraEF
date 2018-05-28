9cb977696ea39146cac089cec47ef2d8dbd4f2bb
adrianolrr <adriano.lrr@gmail.com>
2018-05-28 11:31:17 -0300

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarteiraEF.MvcApplication" Language="C#" %>
---
+++ Models/PessoaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CarteiraEF.Models
+{
+    public class PessoaContext : DbContext
+    {
+        public PessoaContext() : base("DatabaseCarteira")
+        { }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        { }
+    }
+}
